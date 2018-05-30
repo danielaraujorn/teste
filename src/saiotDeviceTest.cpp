86f0f75063da23513b0569778bf0b0d3386c340c
PatricioOliveira <patricio@bct.ect.ufrn.br>
2018-05-30 20:28:02 -0300

    ...

--- src/saiotDeviceTest.cpp
+++ src/saiotDeviceTest.cpp
@@ -1,27 +1,41 @@
 #include <Arduino.h>
 #include <SaIoTDeviceLib.h>
 
+// #include <Wire.h>  //INCLUSÃO DE BIBLIOTECA
+#include <LiquidCrystal_I2C.h> //INCLUSÃO DE BIBLIOTECA
+
 #define qtdSensor 2
 
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // "LiquidCrystal_I2C" 16 colunas 2 linhas
+SaIoTDeviceLib device01;
+
 String serial = "tp02";
-unsigned int timer = 1000;
+unsigned int timer = 100;
 unsigned int contador = 0;
 /* criar um sensor apenas e tentar usar para postar valores */
-
-SaIoTDeviceLib device01;
-void setup(){
-	Serial.println(qtdSensor);
-	boolean acummm = true;
-	device01.addSensor("key01", "litros");
-	Serial.begin(115200);
-	Serial.println("======================");
-	Serial.println(serial);
-	device01.start(serial);
+void setup()
+{
+  pinMode(A0, INPUT);
+  lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("sensores:");
+  lcd.print(qtdSensor);
+  lcd.setCursor(0, 1);
+  lcd.print("serial:");
+  lcd.print(serial);
+  boolean acummm = true;
+  device01.addSensor("key01", "litros");
+  device01.start(serial);
 }
 
-void loop(){
-	delay(timer);
-	Serial.printf("%d\n", ++contador);
-	device01.start(serial);
-
+void loop()
+{
+  delay(timer);
+  lcd.setCursor(0, 0);
+  lcd.print("sensor:  ");
+  lcd.print(digitalRead(A0));
+  Serial.printf("%d\n", ++contador);
+  device01.start(serial);
 }
